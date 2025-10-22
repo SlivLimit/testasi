35d0313c94a539550e52baac32b92826cbb89af3
SlivLimit <ropmek827@gmail.com>
2025-10-22 22:35:01 +0300

    Update averyscar_fix2.js

--- averyscar_fix2.js
+++ averyscar_fix2.js
@@ -250,9 +250,9 @@ const oldRadmirConfig = {
       }
 /* Лого */
       .Old-Fixed-Logo img{
-       width:21.52vh;
+       width:23.52vh;
        height:7.2vh;
-       margin-right:2vh
+       margin-right:3vh
       }
       .Old-Fixed-Bonus{
        background: radial-gradient(93.1% 93.1% at 126.72% 6.9%, #eb00ff 0, #eb00ff00 100%), linear-gradient(129.39deg, #f5be09 30.88%, #e9651b 98.06%);
@@ -762,3 +762,4 @@ body .authorization{background:0 0}#app .authorization{background-image:url(data
   };
 
 AddHud();
+
